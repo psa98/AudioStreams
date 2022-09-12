0f8a4fb883e9a292fe2480872144f0e7e1e69980
psa98 <53611326+psa98@users.noreply.github.com>
2022-09-12 13:41:09 +0300

    Delete StandardChoiceAdapter.kt

--- app/src/main/java/c/ponom/audiostreams/StandardChoiceAdapter.kt
+++
@@ -1,7 +0,0 @@
-package c.ponom.audiostreams
-
-import android.content.Context
-import android.widget.ArrayAdapter
-
-class StandardChoiceAdapter<T>(context: Context, resource: Int, objects: MutableList<T>) :
-    ArrayAdapter<T>(context,resource,objects)
